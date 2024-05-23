d93520af957dd3ee582d3f2501cc09b5b107137d
Ashishkalbande11 <ashishkalbande60>
2024-05-23 08:24:07 +0530

    juice shop

--- src/main/java/com/driver/JuiceShopUI.java
+++ src/main/java/com/driver/JuiceShopUI.java
@@ -33,14 +33,44 @@ public class JuiceShopUI {
 
     private static void printMenu() {
     	//your code goes here
+        System.out.println("Welcome to juice shop :");
+        System.out.println("1. Add Juice :");
+        System.out.println("2. Display summary:");
+        System.out.println("3. Exist :");
+        System.out.println("Enter your choice :");
+
     }
 
     private static void addJuice(Scanner scanner) {
     	//your code goes here
+        System.out.println("Enter juice name :");
+        String name = scanner.next();
+        System.out.println("Enter number of Ingredients:");
+        int size = scanner.nextInt();
+
+        List<String> ingredient = new ArrayList<>();
+        for(int i=0; i<size; i++){
+            System.out.println("Enter" + i+1 + "ingerdient :");
+            ingredient.add(scanner.nextLine());
+        }
+        System.out.println("Enter price :")
+        double price = scanner.nextDouble();
+
+        JuiceDTO juiceDTO = new JuiceDTO(name, ingredient, price);
+        juiceList.add(juiceDTO);
+
     }
 
     private static void displaySummary() {
-        S//your code goes here
+        //your code goes here
+        if(juiceList.isEmpty()){
+            System.out.println("No juice available");
+        }else {
+            System.out.println("Juice Summary");
+            for(JuiceDTO juice : juiceList){
+                System.out.println(juice);
+            }
+        }
     }
 }
 
